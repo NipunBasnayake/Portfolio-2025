e27d126f3bce8336854fe97daa4fa4cf79b742b3
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-11-19 16:20:24 +0530

    4th Commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// --------------------------- Smooth Navigation  ---------------------------
+
 document.querySelectorAll('.nav-link').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,17 +9,23 @@ document.querySelectorAll('.nav-link').forEach(anchor => {
     });
 });
 
+
+// --------------------------- Hover Effect ---------------------------
+
 document.querySelectorAll(".hover-effect").forEach(link => {
     link.addEventListener("mouseover", () => {
         link.style.color = "#F5F5F5";
-        link.style.transform = "scale(1.1)";
+        link.style.transform = "scale(1.1)"; 
     });
 
     link.addEventListener("mouseout", () => {
-        link.style.color = "#C8AD7D";
-        link.style.transform = "scale(1)";
+        link.style.color = "#C8AD7D"; 
+        link.style.transform = "scale(1)"; 
     });
 });
+
+
+// --------------------------- Smooth Scroll ---------------------------
 
 const sections = document.querySelectorAll("section");
 let isScrolling = false;
@@ -37,6 +45,7 @@ window.addEventListener("wheel", (e) => {
         isScrolling = true;
         sections[currentSectionIndex - 1].scrollIntoView({ behavior: "smooth" });
     }
+
     const scrollDuration = 500;
     setTimeout(() => {
         isScrolling = false;
@@ -44,3 +53,28 @@ window.addEventListener("wheel", (e) => {
 });
 
 
+// --------------------------- GitHub Links ---------------------------
+
+document.getElementById('github-btn-1').addEventListener('click', function () {
+    window.open('https://nipunbasnayake.github.io/ToDo/', '_blank');
+});
+
+document.getElementById('github-btn-2').addEventListener('click', function () {
+    window.open('https://github.com/NipunBasnayake/Fashion-Shop', '_blank');
+});
+
+document.getElementById('github-btn-3').addEventListener('click', function () {
+    window.open('https://github.com/NipunBasnayake/Vehicle-Service-Management-System', '_blank');
+});
+
+
+// --------------------------- Text Slide Show ---------------------------
+
+const slides = document.querySelectorAll('.slide-text');
+let currentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = 'inline-block';
+}, 3000);
